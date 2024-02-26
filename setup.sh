a01c4554c7eba5734dc6d432939b671b884356e3
Manuel Parra-Royón <manuparra@gmail.com>
2024-02-26 13:17:34 +0100

    Path set-up setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ export PATH=$PATH:$dir
 export PYTHONPATH=$PYTHONPATH:$dir
 export SINGULARITYENV_PYTHONPATH="$PYTHONPATH:\$PYTHONPATH"
 git config --global --add safe.directory $(dirname $BASH_SOURCE)
+echo "--- Path, Python Path and Singularity Env ready! ---"
